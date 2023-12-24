04780c64d440921b4cf23c2bdde72cdb2788bd58
pointyperson78 <34811003+pointyperson78@users.noreply.github.com>
2023-12-24 18:36:24 +0000

    New interface

---
+++ pages/rummy.py
@@ -0,0 +1,31 @@
+import streamlit as st
+if "actions" not in st.session_state:
+    st.session_state.actions = []
+
+player = st.selectbox(
+    'Player:'
+    ,('Player 1', 'Player 2', 'Player 3', 'Player 4'),
+    index = None
+)
+type = st.selectbox(
+    'Type of Action'
+    ,('Pick up','Put down'),
+    index = None
+)
+num = st.selectbox(
+    'Number:'
+    ,('Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King'),
+    index = None
+)
+suit = st.selectbox(
+    'Suit:'
+    ,('Spades','Clubs','Hearts','Diamonds'),
+    index = None
+)
+
+button = st.button('Add')
+if button:
+   action = f"{player}{type}{num}{suit}"
+   st.session_state.actions.append(action)
+   player, suit, num = [None]*3
+st.write(st.session_state.actions)
